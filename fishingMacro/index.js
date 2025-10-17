35d55f36d8d58339a1da0b9acc578a6df84f85f1
DerJokko <laurin_karig@web.de>
2025-10-18 00:13:25 +0200

    added everything

---
+++ fishingMacro/index.js
@@ -0,0 +1,36 @@
+/// <reference types="../CTAutocomplete" />
+import { DetectFish, reset } from "./features/macro"
+
+const toggleKeybind = new KeyBind("Toggle Macro", Keyboard.KEY_NONE, "Fishing Macro")
+
+toggleKeybind.registerKeyDown(ToggleDown);
+toggleKeybind.registerKeyRelease(ToggleUP);
+
+export let enabled = false
+let KeybindDown = false
+
+function toggleMacro() {
+    if (enabled) {
+        DetectFish.unregister()
+        enabled = false
+        ChatLib.chat(" &d[&bFishingMacro&d]&r &ecurrently&r &c&l[Disabled]")
+    } else {
+        DetectFish.register()
+        enabled = true
+        reset()
+        ChatLib.chat(" &d[&bFishingMacro&d]&r &ecurrently&r &a&l[Enabled]")
+    }
+}
+
+function ToggleDown() {
+    if (!KeybindDown) toggleMacro()
+    KeybindDown = true;
+}
+
+function ToggleUP() {
+    KeybindDown = false
+}
+
+register('command', () => {
+    toggleMacro()
+}).setName('fishingmacro').setAliases('fm')
